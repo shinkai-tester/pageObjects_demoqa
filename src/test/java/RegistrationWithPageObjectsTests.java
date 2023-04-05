9bcfa2145405fc74230843396688a2a65fb5d249
Aleksandra Klimantova <alexandra.klimantowa@gmail.com>
2023-04-05 04:09:40 +0200

    added Allure annotations, attachments, adapted for Jenkins

--- src/test/java/RegistrationWithPageObjectsTests.java
+++ src/test/java/RegistrationWithPageObjectsTests.java
@@ -1,54 +1,76 @@
 import generators.StudentDataGenerator;
+import io.qameta.allure.Link;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import model.StudentData;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 
 public class RegistrationWithPageObjectsTests extends TestBase {
 
 
     @Test
+    @Tag("remote")
+    @Owner("shinkai-tester")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "DEMOQA Practice Form", url = "https://demoqa.com/automation-practice-form")
+    @DisplayName("Fill all data in Student Registration Form and submit it")
     void fillAllDataTest() {
         StudentData newStudentData = StudentDataGenerator.getRandomStudent();
 
-        registrationPage.openPage();
+        step("Open Student Registration Form", () -> {
+            registrationPage.openPage();
+            registrationPage.removeBanAndFooter();
+        });
 
-        registrationPage.removeBanAndFooter();
+        step("Fill all student's data", () -> {
+            registrationPage.setFirstName(newStudentData.getFirstName())
+                    .setLastName(newStudentData.getLastName())
+                    .setEmail(newStudentData.getEmail())
+                    .setGender(newStudentData.getGender())
+                    .setBirthDate(
+                            newStudentData.getBirthDay(),
+                            newStudentData.getBirthMonth(),
+                            newStudentData.getBirthYear()
+                    )
+                    .setPhoto(newStudentData.getPhoto())
+                    .setState(newStudentData.getState())
+                    .setCity(newStudentData.getCity())
+                    .setSubject(newStudentData.getSubject())
+                    .setAddress(newStudentData.getCurrAddress())
+                    .setPhone(newStudentData.getPhone())
+                    .setHobby(newStudentData.getHobby());
+        });
 
-        registrationPage.setFirstName(newStudentData.getFirstName())
-                .setLastName(newStudentData.getLastName())
-                .setEmail(newStudentData.getEmail())
-                .setGender(newStudentData.getGender())
-                .setBirthDate(
-                        newStudentData.getBirthDay(),
-                        newStudentData.getBirthMonth(),
-                        newStudentData.getBirthYear()
-                )
-                .setPhoto(newStudentData.getPhoto())
-                .setState(newStudentData.getState())
-                .setCity(newStudentData.getCity())
-                .setSubject(newStudentData.getSubject())
-                .setAddress(newStudentData.getCurrAddress())
-                .setPhone(newStudentData.getPhone())
-                .setHobby(newStudentData.getHobby());
 
-        registrationPage.submitStudentForm();
+        step("Submit filled Student Registration Form", () -> {
+            registrationPage.submitStudentForm();
+        });
 
-        registrationResultsModal.verifyModalAppears();
+        step("Check after submitting table with all student's data", () -> {
+            registrationResultsModal.verifyModalAppears();
 
-        registrationResultsModal.verifyResult("Student Name", newStudentData.getFirstName() + " " + newStudentData.getLastName())
-                .verifyResult("Student Email", newStudentData.getEmail())
-                .verifyResult("Gender", newStudentData.getGender())
-                .verifyResult("Mobile", newStudentData.getPhone())
-                .verifyResult("Date of Birth",
-                        newStudentData.getBirthDay() + " "
-                                + newStudentData.getBirthMonth()
-                                + "," + newStudentData.getBirthYear())
-                .verifyResult("Subjects", newStudentData.getSubject())
-                .verifyResult("Hobbies", newStudentData.getHobby())
-                .verifyResult("Picture", newStudentData.getPhoto().getName())
-                .verifyResult("Address", newStudentData.getCurrAddress())
-                .verifyResult("State and City", newStudentData.getState() + " " + newStudentData.getCity());
+            registrationResultsModal.verifyResult("Student Name", newStudentData.getFirstName() + " " + newStudentData.getLastName())
+                    .verifyResult("Student Email", newStudentData.getEmail())
+                    .verifyResult("Gender", newStudentData.getGender())
+                    .verifyResult("Mobile", newStudentData.getPhone())
+                    .verifyResult("Date of Birth",
+                            newStudentData.getBirthDay() + " "
+                                    + newStudentData.getBirthMonth()
+                                    + "," + newStudentData.getBirthYear())
+                    .verifyResult("Subjects", newStudentData.getSubject())
+                    .verifyResult("Hobbies", newStudentData.getHobby())
+                    .verifyResult("Picture", newStudentData.getPhoto().getName())
+                    .verifyResult("Address", newStudentData.getCurrAddress())
+                    .verifyResult("State and City", newStudentData.getState() + " " + newStudentData.getCity());
+        });
 
-        registrationResultsModal.closeResultsTable();
-
+        step("Close table with all student's data and end registration", () -> {
+            registrationResultsModal.closeResultsTable();
+        });
     }
 }
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,7 +1,15 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 import pages.components.RegistrationResultsModal;
+
+import java.util.Map;
 
 public class TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
@@ -9,7 +17,30 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1680x1050";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
